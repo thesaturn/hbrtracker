625fdca95cee242f75cdc62aa20f9fd2514da0ec
thesaturn <manuel.bugueno.e@gmail.com>
2023-02-13 16:57:38 -0300

    added style json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import characters from "./characters.json";
+import styles from "./styles.json"
+
+// const CharaRow = ({characters}) => (
+//   <tr key={characters.id}>
+//     {/* <td>{characters.image}</td> */}
+//     <td>{characters.name}</td>
+//   </tr>
+
+// )
+const pathToImg = "https://hbr.quest/hbr/"
+const clickItem = () =>{
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin:"auto",
+        width: 800,
+        paddingTop:"1rem"
+      }
+    }
+    >
+      <h1 className="title">HBR Collection Tracker</h1>
+      <table width="100%">
+        <thead>
+        </thead>
+        <tbody>
+          {styles.map((styles) => (
+            <tr>
+              <td><img src= { pathToImg + styles.strip}></img></td>
+              <td>
+                <div>{styles.chara}</div>
+                <div>{styles.name}</div>
+                <select>
+                  <options>
+                   "1"
+                  </options>
+
+                </select>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
